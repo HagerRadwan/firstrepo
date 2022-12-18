58f44e2582e12d310a43e9b45856371327febd17
Hager Abdulrahman <82414527+HagerRadwan@users.noreply.github.com>
2022-12-18 03:06:50 +0200

    Add files via upload

---
+++ final.py
@@ -0,0 +1,13 @@
+from tkinter import *
+age_app = Tk()
+age_app.title("claculate Age App")
+age_app.geometry("400x200")
+the_text = Label(age_app , text="write your age" ,height=2 , font= ("Arial" , 20))
+the_text.pack()
+
+
+
+
+
+
+age_app.mainloop()
